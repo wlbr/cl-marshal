ca35c1dff4c4ed63da97774e461b1b3ef994ee33
Michael Wolber <mwolber@gmx.de>
2016-12-28 12:43:40 +0100

    Adding special coding-idiom variable

--- coding-idiom.lisp
+++ coding-idiom.lisp
@@ -13,42 +13,43 @@
 
 (in-package :marshal)
 
+(defparameter *idiom-table* '(:array             :array
+                              :hash-table        :hash-table
+                              :coding-identifier :pcode
+                              :list              :list
+                              :dlist             :dlist
+                              :object            :object
+                              :string            :string
+                              :simple-string     :simple-string
+                              :reference         :reference
+                              :coding-release-no 1)
+     "Definition of the vocabulary of the generated serialization. You can
+increase verbosity or compactness by choosing your own 'language'.
+key= access code used inside the programs source code
+value= generated identifier.")
 
 (defmacro coding-idiom (key)
-  "Definition of the vocabulary of the generated serialization. You can 
-  increase verbosity or compactness by choosing your own 'language'"
+  "Definition of the vocabulary of the generated serialization.You can 
+increase verbosity or compactness by choosing your own 'language'. Simply
+define your own vocabulary and redefine the variable ms:*idiom-table*."
   ;;; key= access code used inside the programs source code
   ;;; value= generated identifier.
-  (getf '(:array :array
-                 :hash-table :hash-table
-                 :coding-identifier :pcode
-                 :list :list
-                 :dlist :dlist
-                 :object :object
-                 :string :string
-                 :simple-string :simple-string
-                 :reference :reference
-                 :coding-release-no 1)
-        key))
+  (getf *idiom-table* key))
 
 
-;(defmacro coding-idiom (key)
-;      "Definition of the vocabulary of the generated serialization. You can 
-;increase verbosity or compactness by choosing your own 'language'. 
-;This is a version with shorter keys, that saves an significant amount of 
-;of traffic. Anyway - using NGINX e.g. in front of Hunchentoot to provide an 
-;on the fly gzip compression. may be the better solution, depending on your client.
+;(defparameter *idiom-table* '(:array             :a
+;                              :hash-table        :h
+;                              :coding-identifier :pcode
+;                              :list              :l
+;                              :dlist             :d
+;                              :object            :o
+;                              :string            :s
+;                              :simple-string     :c
+;                              :reference         :r
+;                              :coding-release-no 1.1)
+;     "Definition of the vocabulary of the generated serialization. You can
+;increase verbosity or compactness by choosing your own 'language'.
 ;key= access code used inside the programs source code
-;value= generated identifier."
-;   (getf '(:array :a
-;           :hash-table :h
-;           :coding-identifier :pcode
-;           :list :l
-;           :dlist :d
-;           :object :o
-;           :string :s
-;           :simple-string :c
-;           :reference :r
-;           :coding-release-no 1.1
-;           )
-;     key))
+;value= generated identifier.")
+
+
